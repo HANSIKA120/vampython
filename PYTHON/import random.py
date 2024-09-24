94a82e575794ff42a5d267c0d3885ddc2ec4cc8b
Hansika Gupta <hansikagupta1207@gmail.com>
2024-09-24 11:13:22 +0530

    python program

---
+++ PYTHON/import random.py
@@ -0,0 +1,10 @@
+import random
+print("LUDO")
+ply1=input("enter player's name:  ")
+ply2=input("enter player's name:  ")
+ply3=input("enter player's name:  ")
+ply4=input("enter player's name:  ")
+
+dice1=random.randint(1,6)
+print("dice gives the number",dice1)
+
---
+++ PYTHON/nothing.py
@@ -0,0 +1,18 @@
+dict1={"nothing":1,"something":2}
+dict2={"eating":1,"studying":2,"working out":3,"actually working on something":4,"thinking":5,"overthinking":6}
+print('hello sir, what might you be doing today')
+print(dict1)
+choice=int(input("enter your choice"))
+if choice==1:
+    print("do something u retarded crook")
+elif choice==2:
+    print('what might that be my good sir?')
+    print(dict2)
+    ch=int(input("enter your choice"))
+    if ch in range(1,7):
+        print("please continue")
+    else:
+        print("imma send you to jesus")
+else:
+    print("bitch!!  CHOOSE 1 OR 2")
+print("remember. I DON'T GIVE A FUCK :D")
